9213818ff199113d622f555827b84ce61a7e2f45
Nguyen2024-byte <hennguye@iu.edu>
2024-09-24 16:25:26 -0400

    Add files via upload

---
+++ Week-4/homework-3/README.md
@@ -0,0 +1,5 @@
+Name: Henry Nguyen
+Date: 9/19/2024
+Live URL: https://nguyen2024-byte.github.io/N320/
+Description:
+Assignment 3 "Interest Calculator."
---
+++ Week-4/homework-3/app.js
@@ -0,0 +1,22 @@
+//CMD + Enter to move to the next line.
+//ALT + Up/Down moves lines up and down.
+// Use ALT + Z to break long lines.
+
+// Total = Principle(1+Rate*Time)
+
+// Principle is the amount of money invested.
+// Rate is the interest rate you are receiving
+// Time is the amount of time the money will accrue interest. (In this case, you will use years.)
+
+function formula(Rate, Time) {
+  Total = Principle(1 + Rate * Time);
+}
+
+function tInput() {}
+
+function numStrings() {}
+
+function addNums() {}
+
+// parseFloat
+var num1 = parseFloat(document.getElementById("nInput").value);
