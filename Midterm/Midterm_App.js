f17d8a544e6e42f4be685dbdbcd6de0669d28d60
Nguyen2024-byte <hennguye@iu.edu>
2024-10-08 17:13:08 -0400

    Add files via upload

--- Midterm/Midterm_App.js
+++ Midterm/Midterm_App.js
@@ -2,13 +2,11 @@
 const List = document.getElementById("List");
 
 function addItems() {
-  const item = prompt("Add Item:");
-  document.getElementById(item).innerHTML +=
-    // Gets the input from "item"
-    List.items.push(document.getElementById("item").value);
+  document.getElementById(item).innerHTML += "<li>" + item + "</li>";
+
+  // Gets the input from "item"
 
   // - Each item should be shown added as a li tag in the string ("<li>" + item + "</li>" or `<li>${item}</li>`)
-  document.getElementById("Summary").innerHTML += <li> + item + </li>;
 
   // Updates the "list" ul tag to show all the items currently in the list
   const ul = document.getElementById("List");
@@ -28,8 +26,12 @@ function summarizeList() {
   document.getElementById("Summary").innerHTML +=
     "Total Items: " + totalItems + "<br />";
   // - This should show the total number of items in the list
-  "First Item: " + firstItem + "<br/>";
+
+  document.getElementById("Summary").innerHTML +=
+    "First Item: " + firstItem + "<br/>";
   // - This should show the first item in the list
-  "Last Item: " + lastItem + "<br />";
+
+  document.getElementById("Summary").innerHTML +=
+    "Last Item: " + lastItem + "<br />";
   // - This should show the last item in the list. If the list has only 1 item then this will be the same as the first item.
 }
